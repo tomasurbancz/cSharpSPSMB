7b8ac1e4f5a392eef50f91e493100cb95b763aa3
tomasurbancz <tomas.urban@student.spsmb.cz>
2025-09-18 11:16:59 +0200

    commit

---
+++ src/4rocnik/Maturita/OopExamples/classes/Computer.cs
@@ -0,0 +1,49 @@
+﻿using OopExamples.Interfaces;
+
+namespace OopExamples.classes;
+
+public class Computer : IComputer
+{
+    public IEntity Owner { get; set; }
+    public IMotherBoard MotherBoard { get; set; }
+    public ICPU Cpu { get; set; }
+    public IGPU Gpu { get; set; }
+    public IRAM Ram { get; set; }
+    public IPowerSupply PowerSupply { get; set; }
+    public ICase Case { get; set; }
+    public IMonitor[] Monitors { get; set; }
+    public bool IsOn { get; private set; }
+    public bool IsPersonalPC { get; }
+    public bool IsCompanyPC { get; }
+    
+    public void PowerUp()
+    {
+        IsOn = true;
+    }
+
+    public void ShutDown()
+    {
+        IsOn = false;
+    }
+
+    public void PressPowerButton()
+    {
+        if(!IsOn) PowerUp();
+        else ShutDown();
+    }
+
+    public void Print(string text)
+    {
+        Console.WriteLine(text);
+    }
+
+    public float Compute(string equation)
+    {
+        return 0;
+    }
+
+    public IComputer BuildNewComputer(IComputerConfiguration configuration)
+    {
+        return new ComputerBuilder().BuildFromConfiguration(configuration);
+    }
+}
